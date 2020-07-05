c61466dbd7c9f95354ee5482a0bb5c27ec814847
NgocHieu <zhotro1@gmail.com>
2020-07-05 18:53:41 +0700

    new

--- Beginner/englishapp/admin.py
+++ Beginner/englishapp/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import EnglishAppModel
 
 # Register your models here.
+class EnglishAdmin(admin.ModelAdmin):
+	list_display = ('card_name', 'card_pic', 'card_voice')
+	list_filter = ('card_name',)
+	search_fields = ('card_name',)
 
-admin.site.register(EnglishAppModel)
+admin.site.register(EnglishAppModel, EnglishAdmin)
